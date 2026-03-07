3d13d084cab551bafca58ad4a4c87400b6d4040a
sanabel <sanbelbarham123@gmail.com>
2026-03-07 19:40:55 +0200

    Seed data

---
+++ DAL/Utilis/SeedData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Utilis
+{
+    public class SeedData : ISeedData
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedData(RoleManager<IdentityRole> roleManager)
+        {
+            //use it to access roles table directly and to use the creatAsync and Any methods 
+            _roleManager = roleManager;
+        }
+
+        async Task ISeedData.SeedData()
+        {
+            string[] roles = ["SuperAdmin", "Admin", "User"];
+            if (!_roleManager.Roles.Any())
+            {
+                foreach (var role in roles)
+                {
+                  await  _roleManager.CreateAsync(new IdentityRole(role));
+                }
+               
+            }
+
+
+
+        }
+    }
+}
---
+++ DAL/Utilis/UserSeedData.cs
@@ -0,0 +1,70 @@
+﻿using DAL.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Utilis
+{
+  public class UserSeedData:ISeedData
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserSeedData(UserManager<ApplicationUser>userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task SeedData()
+        {
+    
+            if( ! await _userManager.Users.AnyAsync())
+            {
+                var user1 = new ApplicationUser
+                {
+                    UserName = "sanabel",
+                    FullName = "sanabel barham",
+                    Email = "sanabelbarham123@gmail.com",
+                    EmailConfirmed = true
+
+                };
+                var user2 = new ApplicationUser
+                {
+                    UserName = "malakoot",
+                    FullName = "malakoot barham",
+                    Email = "malakootbarham123@gmail.com",
+                    EmailConfirmed = true
+                };
+                var user3 = new ApplicationUser
+                {
+                    UserName = "sabri",
+                    FullName = "sabri barham",
+                    Email = "sabribarham123@gmail.com",
+                    EmailConfirmed = true
+                };
+                var user4 = new ApplicationUser
+                {
+                    UserName = "yaqoot",
+                    FullName = "yaqoot barham",
+                    Email = "yaqootbarham123@gmail.com",
+                    EmailConfirmed = true
+                };
+
+                var creatUser1 = await _userManager.CreateAsync(user1, "Sanabel@123");
+                var creatUser2 = await _userManager.CreateAsync(user2, "malakoot@123");
+                var creatUser3 = await _userManager.CreateAsync(user3, "sabri@123");
+                var creatUser4 = await _userManager.CreateAsync(user4, "yaqoot@123");
+
+                await _userManager.AddToRoleAsync(user1, "SuperAdmin");
+                await _userManager.AddToRoleAsync(user2, "Admin");
+                await _userManager.AddToRoleAsync(user3, "User");
+                await _userManager.AddToRoleAsync(user4, "User");
+
+            }
+        }
+    }
+
+}
